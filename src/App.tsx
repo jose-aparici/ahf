d53414990bf72b5de3d52eda4c6e1d657b2962eb
jose-aparici <jose.aparici@creditas.com.br>
2020-08-23 09:44:39 +0200

    [Added] Routes with constants

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const App: React.FC = () => <div>App</div>;
+import { AppRoutes } from './pages/App.routes';
+import { AhfMainPage } from './pages/main/main.page';
 
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path={AppRoutes.MainPage} exact component={AhfMainPage} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 export default App;
