a41f1114882cdd1c151f765c5aeb9c7601a8d409
jose-aparici <jose.aparici@creditas.com.br>
2020-08-23 17:01:37 +0200

    [Changed] Carousel container to instantiate items

--- src/modules/carousel/carousel.container.tsx
+++ src/modules/carousel/carousel.container.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SwipeableViews from 'react-swipeable-views';
 
-export const AhfCarouselContainer: React.FC = () => (
-  <div>this is carousel container</div>
-);
+import { Carousel } from 'domain/carousel/carouse.type';
+
+import { AhfCarouselItemContainer } from './item/carousel.item.cotainer';
+
+export const AhfCarouselContainer: React.FC = () => {
+  const [carousel, setCarousel] = useState<Carousel>({ items: [] });
+
+  useEffect(() => {
+    setCarousel({
+      items: [
+        { cards: [{ title: 'title1', description: 'description1' }] },
+        { cards: [{ title: 'title2', description: 'description2' }] },
+      ],
+    });
+  }, []);
+  return (
+    <SwipeableViews enableMouseEvents>
+      {carousel.items.map((item, index) => (
+        <AhfCarouselItemContainer key={index} item={item} />
+      ))}
+    </SwipeableViews>
+  );
+};
