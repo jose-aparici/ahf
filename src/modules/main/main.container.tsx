7534783099c6e768e3eaf7c828f0ba2efa58b2b7
jose-aparici <56715699+jose-aparici@users.noreply.github.com>
2020-08-29 10:42:37 +0200

    [Removed] Main page (#3)

--- src/modules/main/main.container.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-import { AppRoutes } from 'pages/App.routes';
-
-export const AhfMainContainer: React.FC = () => (
-  <Link to={AppRoutes.ParamsGroupsPage}>Parameters group</Link>
-);
--- src/pages/App.routes.ts
+++ src/pages/App.routes.ts
@@ -1,4 +1,3 @@
 export const AppRoutes = {
-  MainPage: '/',
-  ParamsGroupsPage: '/params-groups',
+  ParamsGroupsPage: '/',
 };
