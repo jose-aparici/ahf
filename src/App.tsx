bb774c7fdd7f54bef545071136d82c05059a2c4e
jose-aparici <jose.aparici@creditas.com.br>
2020-08-29 10:40:22 +0200

    [Removed] Main page

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,11 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { AhfParamsGroupsPage } from 'pages/params-groups/params-groups.page';
 
 import { AppRoutes } from './pages/App.routes';
-import { AhfMainPage } from './pages/main/main.page';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path={AppRoutes.MainPage} exact component={AhfMainPage} />
         <Route
           path={AppRoutes.ParamsGroupsPage}
           exact
