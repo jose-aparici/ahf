b9c412f61f244aad8a740b8053c39db9b1051ef8
jose-aparici <jose.aparici@creditas.com.br>
2020-08-31 10:36:50 +0200

    [Added] Store

---
+++ src/store/context.tsx
@@ -0,0 +1,33 @@
+import React, {
+  createContext,
+  Dispatch,
+  ReactElement,
+  ReactNode,
+  useReducer,
+} from 'react';
+
+import { Action } from './actions';
+import { initialState, State } from './initialState';
+import { reducer } from './reducer';
+
+interface Props {
+  state: State;
+  dispatch: Dispatch<Action>;
+}
+export const AhfContext = createContext<Props>({
+  state: initialState,
+  dispatch: () => true,
+});
+
+interface ProviderProps {
+  children: ReactNode;
+}
+
+export const AhfProvider = ({ children }: ProviderProps): ReactElement => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <AhfContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AhfContext.Provider>
+  );
+};
---
+++ src/store/types.ts
@@ -0,0 +1,2 @@
+export const ADD_DATA = 'ADD_DATA';
+export type ADD_DATA = typeof ADD_DATA;
