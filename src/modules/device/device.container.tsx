5d09b7c715a74b621b20b2f8a77e3686b9e22392
jose-aparici <jose.aparici@creditas.com.br>
2020-08-31 18:45:53 +0200

    [Added] get device id router param on device container

--- src/modules/device/device.container.tsx
+++ src/modules/device/device.container.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 import { AppRoutes } from 'pages/App.routes';
 
+interface ParamTypes {
+  deviceId: string;
+}
+
 export const AhfDeviceContainer: React.FC = () => {
+  const { deviceId } = useParams<ParamTypes>();
+
   return (
     <>
       <Link to={AppRoutes.DevicesPage}> Devices</Link>
+      <div>{deviceId}</div>
     </>
   );
 };
