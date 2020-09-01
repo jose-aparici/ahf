bb9d8f117d8bc1ed5cbd61e659e8e2f307271e25
jose-aparici <jose.aparici@creditas.com.br>
2020-09-01 12:03:45 +0200

    [Changed] Devices layout

--- src/modules/devices/devices.container.tsx
+++ src/modules/devices/devices.container.tsx
@@ -1,23 +1,55 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { AhfContext } from 'store/context';
+
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  CircularProgress,
+  Typography,
+} from '@material-ui/core';
 
 import { DeviceInfo } from 'domain/ahf/ahf.types';
 import { AppRoutes } from 'pages/App.routes';
 
+import { useDevicesContainerStyles } from './devices-container.styles';
+
 export const AhfDevicesContainer: React.FC = () => {
+  const classes = useDevicesContainerStyles();
   const { state } = useContext(AhfContext);
+  const history = useHistory();
+
+  const handleClickDevice = (id: number, status: number) => {
+    status > 0 && history.push(`${AppRoutes.DevicesPage}/${id}`);
+  };
 
   return (
-    <>
+    <div className={classes.root}>
       {Object.keys(state.devices).map((deviceKey, index) => {
         const deviceInfo = state.devices[+deviceKey].info as DeviceInfo;
         return (
-          <Link key={index} to={`${AppRoutes.DevicesPage}/${deviceKey}`}>
-            {deviceInfo.ID} Status: {deviceInfo.Status}
-          </Link>
+          <Card
+            key={index}
+            onClick={() => handleClickDevice(deviceInfo.ID, deviceInfo.Status)}
+          >
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="/assets/images/AHFViewer1.ico"
+                title="Contemplative Reptile"
+              ></CardMedia>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {deviceInfo.ID} {deviceInfo.Type}
+                </Typography>
+                {deviceInfo.Status === 0 && <CircularProgress />}
+              </CardContent>
+            </CardActionArea>
+          </Card>
         );
       })}
-    </>
+    </div>
   );
 };
