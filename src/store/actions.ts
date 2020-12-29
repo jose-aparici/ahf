cb178fb5c75da330cfcc016e4d90a6ac64cf38d3
jose-aparici <56715699+jose-aparici@users.noreply.github.com>
2020-12-29 17:47:59 +0100

    [Removed] Socket hook dispatch to store directly (#18)

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,27 +1,13 @@
 import {
+  Command,
   DeviceInfo,
   DeviceParamUpdate,
   DeviceStructure,
 } from 'domain/ahf/ahf.types';
 
-import { DEVICE_INFO, DEVICE_PARAM_UPDATE, DEVICE_STRUCTURE } from './types';
+export type Action = {
+  type: Command;
+  payload: Payload;
+};
 
-export interface DeviceInfoAction {
-  type: DEVICE_INFO;
-  payload: DeviceInfo;
-}
-
-export interface DeviceStructureAction {
-  type: DEVICE_STRUCTURE;
-  payload: DeviceStructure;
-}
-
-export interface DeviceUpdateParamAction {
-  type: DEVICE_PARAM_UPDATE;
-  payload: DeviceParamUpdate;
-}
-
-export type Action =
-  | DeviceInfoAction
-  | DeviceStructureAction
-  | DeviceUpdateParamAction;
+export type Payload = DeviceInfo | DeviceStructure | DeviceParamUpdate;
