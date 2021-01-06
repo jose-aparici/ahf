cc390b0b4da54b6833e226d8e8b456ee39f177b1
jose-aparici <jose.aparici@creditas.com.br>
2021-01-06 17:14:40 +0100

    [Changed] Theme colors

--- src/styles/ahf.colors.ts
+++ src/styles/ahf.colors.ts
@@ -1,7 +1,8 @@
 export const AHF_COLORS = {
   main: {
-    primary: '#11BB77',
-    secondary: '#3377BB',
+    primary: '#000000',
+    secondary: '#e41d18',
+    tertiary: '#111111',
   },
   background: '#f5f5f5',
 };
