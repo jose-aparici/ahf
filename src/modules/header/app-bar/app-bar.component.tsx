d6094aebda8dc8a5642bed1a83e6b13e886ba087
jose-aparici <jose.aparici@creditas.com.br>
2021-01-06 17:16:05 +0100

    [Changed] Use component property on buttons

--- src/modules/header/app-bar/app-bar.component.tsx
+++ src/modules/header/app-bar/app-bar.component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { AppBar, IconButton, Toolbar } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 import HomeIcon from '@material-ui/icons/Home';
 import MenuIcon from '@material-ui/icons/Menu';
 import SettingsIcon from '@material-ui/icons/Settings';
@@ -11,45 +12,69 @@ import { AppRoutes } from 'pages/App.routes';
 import { useAppBarComponentStyles } from './app-bar.component.styles';
 
 interface Props {
-  onSideBarOpen: () => void;
+  onToggleSideBar: () => void;
 }
 export const AhfAppBarComponent: React.FC<Props> = ({
-  onSideBarOpen,
+  onToggleSideBar,
 }: Props) => {
   const classes = useAppBarComponentStyles();
 
   return (
-    <AppBar position="fixed">
-      <Toolbar>
+    <AppBar position="sticky">
+      <Toolbar className={classes.toolBarContainer}>
         <IconButton
           color="inherit"
           aria-label="open drawer"
           edge="start"
-          onClick={onSideBarOpen}
+          onClick={onToggleSideBar}
         >
           <MenuIcon />
         </IconButton>
-        <Link to={AppRoutes.DevicesPage}>Device home</Link>
-        <Link to={AppRoutes.DevicesPage}>Oscilloscope</Link>
-        <Link to={AppRoutes.DevicesPage}>Event log</Link>
-        <Link to={AppRoutes.DevicesPage}>
-          <IconButton
-            edge="start"
-            className={classes.homeButton}
-            aria-label="menu"
-          >
-            <SettingsIcon />
-          </IconButton>
-        </Link>
-        <Link to={AppRoutes.DevicesPage}>
-          <IconButton
-            edge="start"
-            className={classes.homeButton}
-            aria-label="menu"
-          >
-            <HomeIcon />
-          </IconButton>
-        </Link>
+        <div className={classes.navContainer}>
+          <div>
+            <Button
+              component={Link}
+              to={AppRoutes.DevicesPage}
+              color="secondary"
+            >
+              Device home
+            </Button>
+            <Button
+              component={Link}
+              to={AppRoutes.DevicesPage}
+              color="secondary"
+            >
+              Oscilloscope
+            </Button>
+            <Button
+              component={Link}
+              to={AppRoutes.DevicesPage}
+              color="secondary"
+            >
+              Event log
+            </Button>
+          </div>
+
+          <div>
+            <IconButton
+              component={Link}
+              to={AppRoutes.DevicesPage}
+              color="inherit"
+              aria-label="menu"
+            >
+              <SettingsIcon />
+            </IconButton>
+
+            <IconButton
+              component={Link}
+              to={AppRoutes.DevicesPage}
+              color="inherit"
+              aria-label="menu"
+            >
+              <HomeIcon />
+            </IconButton>
+          </div>
+        </div>
       </Toolbar>
     </AppBar>
   );
