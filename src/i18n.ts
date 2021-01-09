faba80b934eae912484006178b632a5c04cfcd07
jose-aparici <jose.aparici@creditas.com.br>
2021-01-09 18:44:23 +0100

    [Changed] Set i18n debug to false

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,13 +2,15 @@ import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
+import { DEFAULT_LANGUAGE } from 'domain/languages/languages.constants';
+
 i18n
   .use(Backend)
   .use(initReactI18next)
   .init({
-    lng: 'en_EN',
-    fallbackLng: 'en_EN',
-    debug: true,
+    lng: DEFAULT_LANGUAGE.locale,
+    fallbackLng: DEFAULT_LANGUAGE.locale,
+    debug: false,
     interpolation: {
       escapeValue: false,
     },
