343c777b0a9b329ec501d790313165b04febd11c
jose-aparici <jose.aparici@creditas.com.br>
2021-01-09 18:44:57 +0100

    [Added] Use language selector

--- src/modules/header/app-bar/app-bar.component.tsx
+++ src/modules/header/app-bar/app-bar.component.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
 import { AppBar, IconButton, Toolbar } from '@material-ui/core';
@@ -7,8 +8,13 @@ import HomeIcon from '@material-ui/icons/Home';
 import MenuIcon from '@material-ui/icons/Menu';
 import SettingsIcon from '@material-ui/icons/Settings';
 
+import {
+  AHF_LANGUAGES,
+  DEFAULT_LANGUAGE,
+} from 'domain/languages/languages.constants';
 import { AppRoutes } from 'pages/App.routes';
 
+import { AhfLanguageSelectorComponent } from '../language-selector/language-selector.component';
 import { useAppBarComponentStyles } from './app-bar.component.styles';
 
 interface Props {
@@ -18,64 +24,74 @@ export const AhfAppBarComponent: React.FC<Props> = ({
   onToggleSideBar,
 }: Props) => {
   const classes = useAppBarComponentStyles();
-
+  const { i18n } = useTranslation();
+  const handleChangeLanguage = (locale = DEFAULT_LANGUAGE.locale) =>
+    i18n.changeLanguage(locale);
   return (
-    <AppBar position="sticky">
-      <Toolbar className={classes.toolBarContainer}>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          edge="start"
-          onClick={onToggleSideBar}
-        >
-          <MenuIcon />
-        </IconButton>
-        <div className={classes.navContainer}>
-          <div>
-            <Button
-              component={Link}
-              to={AppRoutes.DevicesPage}
-              color="secondary"
-            >
-              Device home
-            </Button>
-            <Button
-              component={Link}
-              to={AppRoutes.DevicesPage}
-              color="secondary"
-            >
-              Oscilloscope
-            </Button>
-            <Button
-              component={Link}
-              to={AppRoutes.DevicesPage}
-              color="secondary"
-            >
-              Event log
-            </Button>
-          </div>
+    <>
+      <AppBar>
+        <Toolbar className={classes.toolBarContainer}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={onToggleSideBar}
+          >
+            <MenuIcon />
+          </IconButton>
+          <div className={classes.navContainer}>
+            <div>
+              <Button
+                component={Link}
+                to={AppRoutes.DevicesPage}
+                color="secondary"
+              >
+                Device home
+              </Button>
+              <Button
+                component={Link}
+                to={AppRoutes.DevicesPage}
+                color="secondary"
+              >
+                Oscilloscope
+              </Button>
+              <Button
+                component={Link}
+                to={AppRoutes.DevicesPage}
+                color="secondary"
+              >
+                Event log
+              </Button>
+            </div>
 
-          <div>
-            <IconButton
-              component={Link}
-              to={AppRoutes.DevicesPage}
-              color="inherit"
-              aria-label="menu"
-            >
-              <SettingsIcon />
-            </IconButton>
+            <div>
+              <AhfLanguageSelectorComponent
+                currentLanguage={i18n.language}
+                languages={AHF_LANGUAGES}
+                onChangeLanguage={handleChangeLanguage}
+              />
+              <IconButton
+                component={Link}
+                to={AppRoutes.DevicesPage}
+                color="inherit"
+                aria-label="menu"
+              >
+                <SettingsIcon />
+              </IconButton>
 
-            <IconButton
-              component={Link}
-              to={AppRoutes.DevicesPage}
-              color="inherit"
-              aria-label="menu"
-            >
-              <HomeIcon />
-            </IconButton>
+              <IconButton
+                component={Link}
+                to={AppRoutes.DevicesPage}
+                color="inherit"
+                aria-label="menu"
+              >
+                <HomeIcon />
+              </IconButton>
+            </div>
           </div>
-        </div>
-      </Toolbar>
-    </AppBar>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </>
   );
 };
