7c50594c0daf72da872df78cf769b8cbd5c7d42b
jose-aparici <jose.aparici@creditas.com.br>
2021-01-09 16:30:41 +0100

    [Removed] Uneeded theme import

--- src/modules/header/header.container.styles.ts
+++ src/modules/header/header.container.styles.ts
@@ -1,6 +1,6 @@
-import { makeStyles, Theme } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 
-export const useHeaderContainerStyles = makeStyles((theme: Theme) => ({
+export const useHeaderContainerStyles = makeStyles(() => ({
   root: {
     display: 'flex',
   },
