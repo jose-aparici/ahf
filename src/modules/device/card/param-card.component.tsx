d3e3c33ad2bdd985f2269137abfa1d4563f0853c
jose-aparici <jose.aparici@creditas.com.br>
2021-01-10 10:50:39 +0100

    [Added] Translate params and remove carousel item

--- src/modules/device/card/param-card.component.tsx
+++ src/modules/device/card/param-card.component.tsx
@@ -8,16 +8,20 @@ import { useParamCardComponentStyles } from './param-card.component.styles';
 
 interface Props {
   param: Param;
+  currentLanguage: number;
 }
 
-export const AhfParamCardComponent: React.FC<Props> = ({ param }: Props) => {
+export const AhfParamCardComponent: React.FC<Props> = ({
+  param,
+  currentLanguage,
+}: Props) => {
   const classes = useParamCardComponentStyles();
   return (
     <Card variant="elevation">
       <CardContent className={classes.contentContainer}>
         <div className={classes.infoContainer}>
           <Avatar className={classes.avatar}>{param.ParamID}</Avatar>
-          <Typography>{param.Name[1]}</Typography>
+          <Typography>{param.Name[currentLanguage]}</Typography>
         </div>
         <div className={classes.unitContainer}>
           <Typography component="h1" className={classes.value}>
@@ -25,7 +29,7 @@ export const AhfParamCardComponent: React.FC<Props> = ({ param }: Props) => {
           </Typography>
           <Typography className={classes.unit}>{param.Unit}</Typography>
         </div>
-        <Typography>{param.Description[1]}</Typography>
+        <Typography>{param.Description[currentLanguage]}</Typography>
       </CardContent>
     </Card>
   );
