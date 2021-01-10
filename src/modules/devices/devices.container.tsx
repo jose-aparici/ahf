1c9b3eb78a3cf78bd536c70c65fd8358be1fe783
jose-aparici <jose.aparici@creditas.com.br>
2021-01-10 18:25:28 +0100

    [Changed] Device card circular progress color

--- src/modules/devices/devices.container.tsx
+++ src/modules/devices/devices.container.tsx
@@ -49,7 +49,7 @@ export const AhfDevicesContainer: React.FC = () => {
                 ></Avatar>
                 <Typography>{deviceInfo.Type}</Typography>
                 {deviceInfo.Status === 0 ? (
-                  <CircularProgress size={20} />
+                  <CircularProgress size={20} color="secondary" />
                 ) : (
                   <CheckCircleOutlineIcon htmlColor={'green'} />
                 )}
