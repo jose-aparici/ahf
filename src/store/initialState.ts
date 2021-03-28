3892dabeef26bdbdceb256c44ea97e5497efa799
jose-aparici <jose.aparici@creditas.com.br>
2021-03-28 17:37:28 +0200

    [Changed] Mocked state

--- src/store/initialState.ts
+++ src/store/initialState.ts
@@ -1,8 +1,25 @@
+import { buildDeviceAhf } from '__mocks__/buildDevice';
+
+import { AhfDevice } from 'domain/ahf-device/ahf-device.types';
 import { Device } from 'domain/device/device.types';
+
+import { buildState } from './__mocks__/buildState';
+import { deviceInfoReducer } from './reducer_device_info';
+import { deviceStructureReducer } from './reducer_device_structure';
+
+const deviceAhf: AhfDevice = buildDeviceAhf();
 
 export interface State {
   devices: Record<number, Device>;
 }
+
+export const mockedState: State = deviceStructureReducer(
+  deviceInfoReducer(buildState(), deviceAhf.info),
+  deviceAhf.structure,
+);
+
+console.log(mockedState);
+
 export const initialState: State = {
-  devices: {},
+  devices: mockedState.devices,
 };
