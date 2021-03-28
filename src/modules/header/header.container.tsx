7e29b252542a73556fd720d5ad24160f05352640
jose-aparici <jose.aparici@creditas.com.br>
2021-03-28 12:32:38 +0200

    [Changed] Improve tree

--- src/modules/header/header.container.tsx
+++ src/modules/header/header.container.tsx
@@ -1,11 +1,12 @@
 import React, { FC, useContext, useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import { AhfContext } from 'store/context';
 
 import { AppBar, Toolbar } from '@material-ui/core';
 
 import { pathToBreadCrumbs } from 'domain/breadcrumbs/breadcrumb.utils';
 import { Breadcrumb } from 'domain/breadcrumbs/breadcrumbs.types';
+import { getIdsWithChildren } from 'domain/folder/folder.utils';
 import { AppRoutes } from 'pages/App.routes';
 
 import { AhfBreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
@@ -15,19 +16,23 @@ import { AhfNavigationIconsComponent } from './navigation-icons/navigation-icons
 import { AhfSideBarButtonComponent } from './side-bar-button/side-bar-button.component';
 import { AhfSideBarComponent } from './side-bar/side-bar.component';
 
+interface ParamTypes {
+  deviceId: string;
+}
 export const AhfHeaderContainer: FC = () => {
   const classes = useHeaderContainerStyles();
   const [sideBarOpen, setSideBarOpen] = useState(false);
   const [breadcrumbs, setBreadcrumbs] = useState<Breadcrumb[]>();
   const { state } = useContext(AhfContext);
   const location = useLocation();
+  const { deviceId } = useParams<ParamTypes>();
 
   useEffect(() => {
-    console.log(location.pathname);
     setBreadcrumbs(pathToBreadCrumbs(location.pathname));
   }, [location.pathname]);
 
   const handleToggleSideBar = (): void => setSideBarOpen(!sideBarOpen);
+  console.log('deviceId', deviceId);
 
   return (
     <>
@@ -56,10 +61,16 @@ export const AhfHeaderContainer: FC = () => {
             isOpen={sideBarOpen}
             onToggleSideBar={handleToggleSideBar}
           >
-            <AhfFolderTreeViewComponent
-              devices={state.devices}
-              onToggleSideBar={handleToggleSideBar}
-            />
+            {true && state.devices[1] && state.devices[1].structure && (
+              <AhfFolderTreeViewComponent
+                device={state.devices[1]}
+                foldersExpandedIds={getIdsWithChildren(
+                  state.devices[1].structure,
+                  [],
+                )}
+                onToggleSideBar={handleToggleSideBar}
+              />
+            )}
           </AhfSideBarComponent>
         </>
       )}
