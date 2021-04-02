96cc27cb0f55ebae245eac3ad02e0b30183e433e
jose-aparici <jose.aparici@creditas.com.br>
2021-04-02 17:40:32 +0200

    [Changed] paper style

--- src/modules/folder/folder-main/folder-main.component.styles.ts
+++ src/modules/folder/folder-main/folder-main.component.styles.ts
@@ -3,9 +3,10 @@ import { makeStyles } from '@material-ui/core';
 export const useFolderMainComponentStyles = makeStyles(() => ({
   root: {
     flexGrow: 1,
+    marginTop: '12px',
   },
   paper: {
-    padding: '8px',
+    padding: '12px 10px 4px 10px',
   },
   containerTitle: {
     fontWeight: 'bold',
