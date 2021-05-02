105653f586c35f26be17dcbc671996be1dc9b6fc
jose-aparici <jose.aparici@creditas.com.br>
2021-05-02 19:19:06 +0200

    [Changed] remove device id from breadcrumb

--- src/domain/breadcrumbs/breadcrumb.utils.ts
+++ src/domain/breadcrumbs/breadcrumb.utils.ts
@@ -3,9 +3,9 @@ import { Breadcrumb } from './breadcrumbs.types';
 export const pathToBreadCrumbs = (path: string): Breadcrumb[] => {
   const pathSplitted = path.split('/');
   return pathSplitted
-    .filter((_, index) => index > 1)
+    .filter((_, index) => index > 2)
     .map((item, index) => ({
       label: item,
-      path: pathSplitted.slice(0, index + 3).join('/'),
+      path: pathSplitted.slice(0, index + 4).join('/'),
     }));
 };
