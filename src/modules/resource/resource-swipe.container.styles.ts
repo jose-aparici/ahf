817255fe7f68a9fcab1372ad02b0dc972cbe947f
jose-aparici <jose.aparici@creditas.com.br>
2021-05-02 18:41:23 +0200

    [Added] width to slide

--- src/modules/resource/resource-swipe.container.styles.ts
+++ src/modules/resource/resource-swipe.container.styles.ts
@@ -3,5 +3,6 @@ import { makeStyles } from '@material-ui/core';
 export const useResourceSwipeContainerStyles = makeStyles(() => ({
   slide: {
     minHeight: '395px',
+    width: '100%',
   },
 }));
