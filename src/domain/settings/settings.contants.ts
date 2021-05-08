77608496d84c2d7daa1bed335b7faa197f5edaf5
jose-aparici <jose.aparici@creditas.com.br>
2021-05-08 19:04:21 +0200

    [Added] tabs constants and translations

---
+++ src/domain/settings/settings.contants.ts
@@ -0,0 +1,9 @@
+import { Tab } from './setting.types';
+
+export const SETTING_TABS: Tab[] = [
+  { label: 'SETTINGS.TABS.SYSTEM.LABEL' },
+  { label: 'SETTINGS.TABS.MODBUS_HMI.LABEL' },
+  { label: 'SETTINGS.TABS.MODBUS_AHF.LABEL' },
+  { label: 'SETTINGS.TABS.ETHERNET.LABEL' },
+  { label: 'SETTINGS.TABS.LANGUAGE.LABEL' },
+];
--- src/modules/settings/settings.container.tsx
+++ src/modules/settings/settings.container.tsx
@@ -1,3 +1,17 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-export const AhfSettingsContainer: React.FC = () => <div>hola</div>;
+import { Tab, Tabs } from '@material-ui/core';
+
+import { SETTING_TABS } from 'domain/settings/settings.contants';
+
+export const AhfSettingsContainer: React.FC = () => {
+  const { t } = useTranslation();
+  return (
+    <Tabs value={0} onChange={() => 0}>
+      {SETTING_TABS.map((tab) => (
+        <Tab key={tab.label} label={t(tab.label)} />
+      ))}
+    </Tabs>
+  );
+};
