dfab22ecbfc1085eea577bc65c44b43589ca99a0
jose-aparici <jose.aparici@creditas.com.br>
2021-05-23 19:49:17 +0200

    [Added] events path constant

--- src/domain/breadcrumbs/breadcrumb.utils.ts
+++ src/domain/breadcrumbs/breadcrumb.utils.ts
@@ -1,3 +1,5 @@
+import { EVENTS } from 'pages/App.routes';
+
 import { Breadcrumb } from './breadcrumbs.types';
 
 export const pathToBreadCrumbs = (
@@ -8,8 +10,7 @@ export const pathToBreadCrumbs = (
   const breadCrumbs = pathSplitted
     .filter((_, index) => index > 2)
     .map((item, index) => {
-      if (index === 1 && item === 'events') {
-        console.log(item);
+      if (index === 1 && item === EVENTS) {
         return {
           label: 'Event Logs',
           path: pathSplitted.slice(0, index + 4).join('/'),
@@ -21,7 +22,7 @@ export const pathToBreadCrumbs = (
       };
     });
 
-  if (path.indexOf('events') >= 0 && fileName.length > 0) {
+  if (path.indexOf(EVENTS) >= 0 && fileName.length > 0) {
     breadCrumbs.push({
       label: fileName,
       path: breadCrumbs[breadCrumbs.length - 1].path,
--- src/pages/App.routes.ts
+++ src/pages/App.routes.ts
@@ -1,10 +1,11 @@
 export const DEVICES = 'devices';
 export const FOLDER = 'folder';
 export const PARAM = 'param';
+export const EVENTS = 'events';
 
 export const AppRoutes = {
   MainPage: '/',
   DevicesPage: `/${DEVICES}`,
   ResourcePage: `/${DEVICES}/:deviceId/:folderName*`,
-  EventsPage: `/${DEVICES}/:deviceId/:folderName/events`,
+  EventsPage: `/${DEVICES}/:deviceId/:folderName/${EVENTS}`,
 };
