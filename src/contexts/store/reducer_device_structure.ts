ffb1d87055f7219811bf1f312d299e24f404fe61
jose-aparici <jose.aparici@creditas.com.br>
2021-06-01 20:08:58 +0200

    [Changed] not change language when device arrives

--- src/contexts/store/reducer_device_structure.ts
+++ src/contexts/store/reducer_device_structure.ts
@@ -1,11 +1,9 @@
 import { flatten } from 'flattree';
-import i18n from 'i18n';
 
 import { AhfDeviceStructure } from 'domain/ahf-device/ahf-device.types';
 import { AhfFolderData } from 'domain/ahf-folder/ahf-folder.types';
 import { AhfParam, AhfParamType } from 'domain/ahf-param/ahf-param.types';
 import { Folder } from 'domain/folder/folder.types';
-import { AHF_LANGUAGES } from 'domain/languages/languages.constants';
 import { Param, ParamType } from 'domain/param/param.types';
 
 import { AppRoutes } from '../../pages/App.routes';
@@ -50,9 +48,6 @@ const transformAhfParamTypeToParamType = (
 
 const transformAhfParamsToParam = (ahfParams: AhfParam[]): Param[] => {
   return ahfParams.map((ahfParam) => {
-    if (ahfParam.ParamID === 200 && ahfParam.Value) {
-      i18n.changeLanguage(AHF_LANGUAGES[ahfParam.Value as number].locale);
-    }
     return ({
       accessType: ahfParam.AccessType,
       description: ahfParam.Description,
