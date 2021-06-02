2eedfb3c88a106dc0d16ee25e6e8e6799a44fdee
jose-aparici <jose.aparici@creditas.com.br>
2021-06-02 19:36:49 +0200

    [Changed] remove unneeded method

--- src/domain/folder/folder.utils.ts
+++ src/domain/folder/folder.utils.ts
@@ -1,14 +1,5 @@
 import { Folder } from './folder.types';
 
-// TODO remove this
-export const findFolderIndexByName = (
-  folderNames: string[],
-  name: string,
-): number => {
-  const indexFound = folderNames.findIndex((folderName) => folderName === name);
-  return indexFound >= 0 ? indexFound : 0;
-};
-
 export const getIdsWithChildren = (
   folder: Folder,
   folderIds: string[],
