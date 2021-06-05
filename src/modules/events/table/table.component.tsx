33fb8743e74fa2807ded79a0a75126e96cb77139
jose-aparici <56715699+jose-aparici@users.noreply.github.com>
2021-06-05 18:35:27 +0200

    [Added] virtualized table (#47)

--- src/modules/events/table/table.component.tsx
+++ src/modules/events/table/table.component.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
 import React from 'react';
+import { FixedSizeList as List } from 'react-window';
 
 import { Grid, Typography } from '@material-ui/core';
 import CancelIcon from '@material-ui/icons/Cancel';
@@ -19,51 +20,63 @@ const AhfTableComponent: React.FC<Props> = ({ rows }: Props) => {
   const classes = useTableComponentStyles();
 
   return (
-    <>
-      {rows.map((row, index) => {
-        return (
-          <Alert
-            key={index}
-            severity={
-              row.type === LogType.STATUS
-                ? 'success'
-                : ((row.type as unknown) as Color)
-            }
-            classes={{
-              root: classes.alertRoot,
-              message: classes.row,
-              standardError: classes.standardError,
-              standardInfo: classes.standardInfo,
-              standardSuccess: classes.standardSuccess,
-              standardWarning: classes.standardWarning,
-            }}
-            iconMapping={{
-              success: <StarBorderIcon fontSize="inherit" />,
-              error: <CancelIcon fontSize="inherit" />,
-              info: <InfoIcon fontSize="inherit" />,
-              warning: <WarningIcon fontSize="inherit" />,
-            }}
-          >
-            <Grid container spacing={1}>
-              <Grid item xs={1}>
-                <Typography className={classes.text}>{row.date}</Typography>
-              </Grid>
-              <Grid item xs={1}>
-                <Typography className={classes.text}>{row.time}</Typography>
-              </Grid>
-              <Grid item xs={7}>
-                <Typography className={classes.text}>{row.message}</Typography>
-              </Grid>
-              <Grid item xs={3} container alignContent="flex-end">
-                <Typography className={clsx(classes.text, classes.textRight)}>
-                  {row.operatingHours}
-                </Typography>
-              </Grid>
+    <List
+      height={784}
+      itemCount={rows.length}
+      itemSize={37}
+      width={'100%'}
+      itemData={rows}
+      className={classes.row}
+    >
+      {({ data, index, style }) => (
+        <Alert
+          key={index}
+          style={style}
+          severity={
+            data[index].type === LogType.STATUS
+              ? 'success'
+              : ((data[index].type as unknown) as Color)
+          }
+          classes={{
+            root: classes.alertRoot,
+            message: classes.row,
+            standardError: classes.standardError,
+            standardInfo: classes.standardInfo,
+            standardSuccess: classes.standardSuccess,
+            standardWarning: classes.standardWarning,
+          }}
+          iconMapping={{
+            success: <StarBorderIcon fontSize="inherit" />,
+            error: <CancelIcon fontSize="inherit" />,
+            info: <InfoIcon fontSize="inherit" />,
+            warning: <WarningIcon fontSize="inherit" />,
+          }}
+        >
+          <Grid container spacing={1}>
+            <Grid item xs={1}>
+              <Typography className={classes.text}>
+                {data[index].date}
+              </Typography>
             </Grid>
-          </Alert>
-        );
-      })}
-    </>
+            <Grid item xs={1}>
+              <Typography className={classes.text}>
+                {data[index].time}
+              </Typography>
+            </Grid>
+            <Grid item xs={7}>
+              <Typography className={classes.text}>
+                {data[index].message}
+              </Typography>
+            </Grid>
+            <Grid item xs={3} container alignContent="flex-end">
+              <Typography className={clsx(classes.text, classes.textRight)}>
+                {data[index].operatingHours}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Alert>
+      )}
+    </List>
   );
 };
 
