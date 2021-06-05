90ac4141cff44c45b9e616a97c4d30e2b5422cc4
jose-aparici <jose.aparici@creditas.com.br>
2021-06-05 16:52:38 +0200

    [Changed] remove resource types

--- src/modules/resource/store/reducer.ts
+++ src/modules/resource/store/reducer.ts
@@ -1,11 +1,11 @@
 import { AhfParamRead } from 'domain/ahf-param/ahf-param.types';
+import { AhfCommand } from 'domain/ahf/ahf.types';
 import { Action } from 'domain/app/app.types';
 import { Folder } from 'domain/folder/folder.types';
-import { Resource } from 'domain/resource/resource.type';
+import { Resource, ResourceCommand } from 'domain/resource/resource.type';
 
 import { paramReadReducer } from './reducer_param_read';
 import { resourceChangeReducer } from './reducer_resource_change';
-import { PARAM_READ, RESOURCE_CHANGE } from './types';
 
 export interface State {
   folder: Folder;
@@ -16,9 +16,9 @@ export const reducer = (state: State, action: Action): State => {
   const { type, payload } = action;
 
   switch (type) {
-    case RESOURCE_CHANGE:
+    case ResourceCommand.RESOURCE_CHANGE:
       return resourceChangeReducer(state, payload as Resource);
-    case PARAM_READ:
+    case AhfCommand.PARAM_DETAIL:
       return paramReadReducer(state, payload as AhfParamRead);
 
     default:
