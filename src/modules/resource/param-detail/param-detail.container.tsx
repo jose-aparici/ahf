90a54d2dc76f65a52957ee398711275dc861141d
jose-aparici <jose.aparici@creditas.com.br>
2021-06-06 07:56:27 +0200

    [Changed] use shared component

--- src/modules/resource/param-detail/param-detail.container.tsx
+++ src/modules/resource/param-detail/param-detail.container.tsx
@@ -14,7 +14,6 @@ import { useTranslation } from 'react-i18next';
 
 import {
   Avatar,
-  Card,
   CardContent,
   CardHeader,
   FormControl,
@@ -30,6 +29,7 @@ import { findLanguageByLocale } from 'domain/languages/languages.utils';
 import { Severity } from 'domain/notification/notification.types';
 import { AccessType, Param } from 'domain/param/param.types';
 import { getParamValue, stringToParamValue } from 'domain/param/param.utils';
+import { AhfCardFullPageComponent } from 'modules/shared/cards/full-page/card-full-page.component';
 
 import { AhfParamEditContainerMemoized } from '../../shared/param-edit/param-edit.container';
 import { useParamDetailContainerStyles } from './param-detail.container.styles';
@@ -130,83 +130,77 @@ export const AhfParamDetailContainer: React.FC<Props> = ({ param }: Props) => {
 
   return (
     <>
-      <Grid container className={classes.gridContainer}>
-        <Grid item xs={12}>
-          <Card variant="elevation" className={classes.cardContainer}>
-            <CardHeader
-              className={classes.cardHeader}
-              avatar={
-                <Avatar className={classes.avatar} variant="square">
-                  {param.paramId}
-                </Avatar>
-              }
-              title={
-                <Typography variant="h3" className={classes.title}>
-                  {param.name[currentLanguage]}
-                </Typography>
-              }
-              action={
-                param.accessType === AccessType.READ_WRITE && (
-                  <IconButton aria-label="settings">
-                    <EditIcon />
-                  </IconButton>
-                )
-              }
-            ></CardHeader>
-            <CardContent>
-              <Grid item container>
-                <FormControl fullWidth>
-                  <TextField
-                    label={t('RESOURCE.PARAM_DETAIL.FIELDS.VALUE.LABEL')}
-                    value={
-                      param.value !== undefined
-                        ? `${getParamValue(param, currentLanguage)} ${
-                            param.unit
-                          }`
-                        : ' -- '
-                    }
-                    onClick={handleClickInput}
-                    placeholder=""
-                    InputProps={{
-                      classes: {
-                        input: classes.value,
-                      },
-                    }}
-                    InputLabelProps={{
-                      shrink: true,
-                      classes: {
-                        root: classes.label,
-                      },
-                    }}
-                  />
-                </FormControl>
-              </Grid>
-              <Grid item container>
-                <FormControl disabled fullWidth>
-                  <TextField
-                    className={classes.description}
-                    label={t('RESOURCE.PARAM_DETAIL.FIELDS.DESCRIPTION.LABEL')}
-                    value={param.description[currentLanguage]}
-                    placeholder=""
-                    multiline
-                    InputProps={{
-                      classes: {
-                        input: classes.value,
-                      },
-                    }}
-                    InputLabelProps={{
-                      shrink: true,
-                      classes: {
-                        root: classes.label,
-                      },
-                    }}
-                  />
-                </FormControl>
-              </Grid>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
+      <AhfCardFullPageComponent>
+        <CardHeader
+          className={classes.cardHeader}
+          avatar={
+            <Avatar className={classes.avatar} variant="square">
+              {param.paramId}
+            </Avatar>
+          }
+          title={
+            <Typography variant="h3" className={classes.title}>
+              {param.name[currentLanguage]}
+            </Typography>
+          }
+          action={
+            param.accessType === AccessType.READ_WRITE && (
+              <IconButton aria-label="settings">
+                <EditIcon />
+              </IconButton>
+            )
+          }
+        ></CardHeader>
+        <CardContent>
+          <Grid item container>
+            <FormControl fullWidth>
+              <TextField
+                label={t('RESOURCE.PARAM_DETAIL.FIELDS.VALUE.LABEL')}
+                value={
+                  param.value !== undefined
+                    ? `${getParamValue(param, currentLanguage)} ${param.unit}`
+                    : ' -- '
+                }
+                onClick={handleClickInput}
+                placeholder=""
+                InputProps={{
+                  classes: {
+                    input: classes.value,
+                  },
+                }}
+                InputLabelProps={{
+                  shrink: true,
+                  classes: {
+                    root: classes.label,
+                  },
+                }}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item container>
+            <FormControl disabled fullWidth>
+              <TextField
+                className={classes.description}
+                label={t('RESOURCE.PARAM_DETAIL.FIELDS.DESCRIPTION.LABEL')}
+                value={param.description[currentLanguage]}
+                placeholder=""
+                multiline
+                InputProps={{
+                  classes: {
+                    input: classes.value,
+                  },
+                }}
+                InputLabelProps={{
+                  shrink: true,
+                  classes: {
+                    root: classes.label,
+                  },
+                }}
+              />
+            </FormControl>
+          </Grid>
+        </CardContent>
+      </AhfCardFullPageComponent>
       {openEditModal && (
         <AhfParamEditContainerMemoized
           avatarTitle={param.paramId.toString()}
