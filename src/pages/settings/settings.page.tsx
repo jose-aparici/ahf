6e7114668282b111358d99ffe81ab2cb116f6667
jose-aparici <jose.aparici@creditas.com.br>
2021-06-09 07:12:57 +0200

    [Changed] stop update on page

--- src/pages/settings/settings.page.tsx
+++ src/pages/settings/settings.page.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { AhfSettingsContainer } from 'modules/settings/settings.container';
+import { useSocketHook } from 'modules/shared/hooks/socket-hook';
 import { AhfPage } from 'pages/ahf.page';
 
 export const AhfSettingsPage: React.FC = () => {
+  const { stopUpdate } = useSocketHook();
+
+  useEffect(() => {
+    stopUpdate();
+  }, [stopUpdate]);
+
   return (
     <AhfPage>
       <AhfSettingsContainer />
