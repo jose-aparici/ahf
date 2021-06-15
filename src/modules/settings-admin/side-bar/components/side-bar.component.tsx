8b14dcf13b778198407e0f2687bdbe7ab9fd81fb
jose-aparici <jose.aparici@creditas.com.br>
2021-06-15 18:17:25 +0200

    [Added] update buttons

--- src/modules/settings-admin/side-bar/components/side-bar.component.tsx
+++ src/modules/settings-admin/side-bar/components/side-bar.component.tsx
@@ -26,6 +26,28 @@ export const AhfSideBarComponent: React.FC<Props> = ({
         <Typography className={classes.iconsSectionTitle} variant="h3">
           {t('SETTINGS_ADMIN.SIDEBAR.UPDATE.TITLE')}
         </Typography>
+        <Button
+          className={clsx(classes.eventsButton, classes.openSavedEventsButton)}
+          variant="outlined"
+          size="large"
+          startIcon={<FolderOpenIcon />}
+          onClick={onOpenList}
+        >
+          <Typography variant="h5">
+            {t('SETTINGS_ADMIN.SIDEBAR.ACTIONS.BUTTONS.OPEN')}
+          </Typography>
+        </Button>
+        <Button
+          className={clsx(classes.eventsButton, classes.saveEventsButton)}
+          variant="outlined"
+          size="large"
+          startIcon={<SaveIcon />}
+          onClick={onOpenSaveFileName}
+        >
+          <Typography variant="h5">
+            {t('SETTINGS_ADMIN.SIDEBAR.ACTIONS.BUTTONS.SAVE')}
+          </Typography>
+        </Button>
       </div>
       <div className={classes.iconsSection}>
         <Typography className={classes.iconsSectionTitle} variant="h3">
