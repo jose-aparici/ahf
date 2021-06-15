e044912cc2cc8ab248751dea346f31e7597e6b33
jose-aparici <jose.aparici@creditas.com.br>
2021-06-15 20:16:22 +0200

    [Added] top buttons

--- src/modules/oscilloscope/oscilloscope.container.tsx
+++ src/modules/oscilloscope/oscilloscope.container.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import { AhfSideBarContainer } from './side-bar/side-bar.container';
+import { AhfTopButtonsComponent } from './top-buttons/top-buttons.component';
 
 export const AhfOScilloscopeContainer: React.FC = () => {
   return (
     <>
+      <AhfTopButtonsComponent />
       <AhfSideBarContainer />
     </>
   );
---
+++ src/modules/oscilloscope/top-buttons/top-buttons.component.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const AhfTopButtonsComponent: React.FC = () => {
+  return <div>hola</div>;
+};
