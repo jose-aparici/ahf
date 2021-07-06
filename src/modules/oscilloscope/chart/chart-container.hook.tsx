92c8837111a36a6c7c673d4b5f9560074e23f880
jose-aparici <jose.aparici@creditas.com.br>
2021-07-06 08:01:14 +0100

    [Added] backdrop

--- src/modules/oscilloscope/chart/chart-container.hook.tsx
+++ src/modules/oscilloscope/chart/chart-container.hook.tsx
@@ -1,8 +1,10 @@
+import { AhfBackdropContext } from 'contexts/backdrop/context';
 import { AhfContext } from 'contexts/store/context';
 import { useContext, useEffect, useState } from 'react';
 
 import { Data } from 'domain/chart/chart.types';
 import { OscilloscopeMode } from 'domain/oscilloscope-settings/oscilloscope-settings.types';
+import { Colors } from 'domain/oscilloscope/oscilloscope.constants';
 
 interface ChartContainerHook {
   data: Data | undefined;
@@ -11,6 +13,11 @@ interface ChartContainerHook {
 export const useChartContainer = (): ChartContainerHook => {
   const { state } = useContext(AhfContext);
   const [data, setData] = useState<Data>();
+  const { closeBackdrop, openBackdrop } = useContext(AhfBackdropContext);
+
+  useEffect(() => {
+    openBackdrop(false);
+  }, [openBackdrop]);
 
   useEffect(() => {
     if (state.oscilloscope.data) {
@@ -21,15 +28,18 @@ export const useChartContainer = (): ChartContainerHook => {
           labels: state.oscilloscope.data?.xAxis.xTime.map((label) =>
             label.toString(),
           ),
-          datasets: state.oscilloscope.data.yAxis.channels.map((channel) => {
-            return {
-              data: channel.time,
-              fill: false,
-              backgroundColor: 'rgb(255, 99, 132)',
-              borderColor: 'rgba(255, 99, 132, 0.2)',
-            };
-          }),
+          datasets: state.oscilloscope.data.yAxis.channels.map(
+            (channel, index) => {
+              return {
+                data: channel.time,
+                fill: false,
+                backgroundColor: Colors[index],
+                borderColor: Colors[index],
+              };
+            },
+          ),
         };
+        closeBackdrop();
         return setData(data);
       } else {
         const data: Data = {
@@ -45,10 +55,15 @@ export const useChartContainer = (): ChartContainerHook => {
             };
           }),
         };
+        closeBackdrop();
         return setData(data);
       }
     }
-  }, [state.oscilloscope.data, state.oscilloscope.settings.mode]);
+  }, [
+    state.oscilloscope.data,
+    state.oscilloscope.settings.mode,
+    closeBackdrop,
+  ]);
 
   return { data };
 };
